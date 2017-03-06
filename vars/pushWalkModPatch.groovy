20189a7678aff480a740d6ce39694da4f2494c45
Raquel Pau <raquelpau@gmail.com>
2017-03-06 19:01:33 +0100

    rebase

--- vars/pushWalkModPatch.groovy
+++ vars/pushWalkModPatch.groovy
@@ -5,10 +5,10 @@
  */
 def call(String branch = 'master') {
 
-    sh "git pull origin $branch"
-
     sh 'git commit -a --amend -m "Fixing style violations"'
 
+    sh "git pull --rebase origin $branch"
+    
     sh "git push origin HEAD:$branch"
 
 
